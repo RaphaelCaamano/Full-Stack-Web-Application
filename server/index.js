963c3ad8019f5ff7f0768035d9a683c30b474661
Raphael Caamano <155185862+RaphaelCaamano@users.noreply.github.com>
2025-04-17 11:20:44 -0400

    Add files via upload

--- server/index.js
+++ server/index.js
@@ -1,1 +1,24 @@
+// This file is defining our express server and running it on port 3000
 
+const express = require('express')
+const app = express()
+const port = 3000
+
+// middleware
+app.use(express.json()) // allows us to parse incoming JSON data from a client
+app.use(express.urlencoded()) // allows us to parse incoming URL Encoded data 
+
+// add middleware for static files
+app.use(express.static('public'))
+
+
+app.get('/test/api', (req, res)=>{
+    res.send({
+        message: "Success! Server is operational"
+    })
+})
+
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
